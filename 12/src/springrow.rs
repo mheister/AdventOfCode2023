6317bda6db8dd314117164c1d03e2222ac0f5919
Marcel Heistermann <mheistermann@gmx.de>
2023-12-20 18:22:24 +0100

    rewrite; still to slow

--- 12/src/springrow.rs
+++ 12/src/springrow.rs
@@ -2,8 +2,6 @@ use std::iter::repeat;
 
 use itertools::Itertools;
 
-use crate::gaparangement::GapArrangement;
-
 #[derive(Debug, PartialEq, Eq)]
 pub struct SpringRow {
     pub row: String,
@@ -12,34 +10,54 @@ pub struct SpringRow {
 
 impl SpringRow {
     pub fn count_possibilities(&self) -> usize {
-        GapArrangement {
-            n_gaps: (self.groups.len() + 1).try_into().unwrap(),
-            total_gap_len: (self.row.len()
-                - self.groups.iter().cloned().sum::<u32>() as usize)
-                .try_into()
-                .unwrap(),
+        Self::count_possibilities_fast_internal(&self.row, &self.groups)
+    }
+
+    fn count_possibilities_fast_internal(row: &str, groups: &[u32]) -> usize {
+        if groups.is_empty() {
+            return 1;
         }
-        .into_iter()
-        .map(|gaps| {
-            let mut row = repeat('.')
-                .take(*gaps.first().unwrap_or(&0) as usize)
-                .collect::<String>();
-            for (&grp, &gap) in self.groups.iter().zip(gaps.iter().skip(1)) {
-                row.extend(repeat('#').take(grp as usize));
-                row.extend(repeat('.').take(gap as usize));
+        if row.is_empty() {
+            return 0;
+        }
+        let grp0 = groups[0] as usize;
+        let grps = &groups[1..];
+        let pos_max: usize = row.len().saturating_sub(
+            grp0 + (grps.len().saturating_sub(1)) + grps.iter().sum::<u32>() as usize,
+        );
+        // group begins latest at first known broken spring
+        let pos_max = std::cmp::min(
+            pos_max,
+            row.chars().position(|c| c == '#').unwrap_or(pos_max),
+        );
+        let mut sum = 0;
+        for i in 0..=pos_max {
+            if i > row.len() {
+                dbg!(row, i, groups);
             }
-            row
-        })
-        .filter(|row_canditate| {
-            self.row
-                .chars()
-                .zip(row_canditate.chars())
-                .all(|(r, c)| match (r, c) {
-                    ('?', _) => true,
-                    (r, c) => r == c,
-                })
-        })
-        .count()
+            if i > 0 && row.chars().nth(i - 1).unwrap() == '#' {
+                // need a gap to begin a group
+                continue;
+            }
+            if row.chars().nth(i + grp0).unwrap_or('.') == '#' {
+                // need a gap towards the next group
+                continue;
+            }
+            if row.chars().skip(i).take(grp0).any(|c| c == '.') {
+                continue;
+            }
+            if row.len() < i + grp0 {
+                continue;
+            }
+            if row.len() <= i + grp0 + 1 {
+                if grps.is_empty() {
+                    sum += 1;
+                }
+                continue;
+            }
+            sum += Self::count_possibilities_fast_internal(&row[i + grp0 + 1..], grps);
+        }
+        sum
     }
 
     pub fn unfold(&self) -> Self {
@@ -52,33 +70,116 @@ impl SpringRow {
     }
 }
 
-#[test]
-pub fn count_possibilities_1st_example_row() {
-    let row = SpringRow {
-        row: "???.###".to_string(),
-        groups: vec![1, 1, 3],
-    };
-    assert_eq!(row.count_possibilities(), 1);
-}
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-#[test]
-pub fn count_possibilities_last_example_row() {
-    let row = SpringRow {
-        row: "?###????????".to_string(),
-        groups: vec![3, 2, 1],
-    };
-    assert_eq!(row.count_possibilities(), 10);
-}
+    #[test]
+    pub fn count_possibilities_bug1() {
+        let row = SpringRow {
+            row: "????###????????".to_string(),
+            groups: vec![3, 2, 1],
+        };
+        assert_eq!(row.count_possibilities(), 10);
+    }
 
-#[test]
-pub fn unfold_1st_example_row() {
-    let row = SpringRow {
-        row: "???.###".to_string(),
-        groups: vec![1, 1, 3],
-    };
-    let unfolded = SpringRow {
-        row: "???.###????.###????.###????.###????.###".to_string(),
-        groups: vec![1, 1, 3, 1, 1, 3, 1, 1, 3, 1, 1, 3, 1, 1, 3],
-    };
-    assert_eq!(row.unfold(), unfolded)
+    #[test]
+    pub fn count_possibilities_1st_example_row() {
+        let row = SpringRow {
+            row: "???.###".to_string(),
+            groups: vec![1, 1, 3],
+        };
+        assert_eq!(row.count_possibilities(), 1);
+    }
+
+    #[test]
+    pub fn count_possibilities_4th_example_row() {
+        let row = SpringRow {
+            row: "????.#...#...".to_string(),
+            groups: vec![4, 1, 1],
+        };
+        assert_eq!(row.count_possibilities(), 1);
+    }
+
+    #[test]
+    pub fn count_possibilities_last_example_row() {
+        let row = SpringRow {
+            row: "?###????????".to_string(),
+            groups: vec![3, 2, 1],
+        };
+        assert_eq!(row.count_possibilities(), 10);
+    }
+
+    #[test]
+    pub fn unfold_1st_example_row() {
+        let row = SpringRow {
+            row: "???.###".to_string(),
+            groups: vec![1, 1, 3],
+        };
+        let unfolded = SpringRow {
+            row: "???.###????.###????.###????.###????.###".to_string(),
+            groups: vec![1, 1, 3, 1, 1, 3, 1, 1, 3, 1, 1, 3, 1, 1, 3],
+        };
+        assert_eq!(row.unfold(), unfolded)
+    }
+
+    #[test]
+    pub fn count_possibilities_unfolded_1st_example_row() {
+        let row = SpringRow {
+            row: "???.###".to_string(),
+            groups: vec![1, 1, 3],
+        }
+        .unfold();
+        assert_eq!(row.count_possibilities(), 1);
+    }
+
+    #[test]
+    pub fn count_possibilities_unfolded_2nd_example_row() {
+        let row = SpringRow {
+            row: ".??..??...?##.".to_string(),
+            groups: vec![1, 1, 3],
+        }
+        .unfold();
+        assert_eq!(row.count_possibilities(), 16384);
+    }
+
+    #[test]
+    pub fn count_possibilities_unfolded_3rd_example_row() {
+        let row = SpringRow {
+            row: "?#?#?#?#?#?#?#?".to_string(),
+            groups: vec![1, 3, 1, 6],
+        }
+        .unfold();
+        assert_eq!(row.count_possibilities(), 1);
+    }
+
+    #[test]
+    pub fn count_possibilities_unfolded_unfolded_4th_example_row() {
+        let row = SpringRow {
+            row: "????.#...#...".to_string(),
+            groups: vec![4, 1, 1],
+        }
+        .unfold();
+        assert_eq!(row.count_possibilities(), 16);
+    }
+
+    #[test]
+    pub fn count_possibilities_unfolded_unfolded_5th_example_row() {
+        let row = SpringRow {
+            row: "????.######..#####.".to_string(),
+            groups: vec![1, 6, 5],
+        }
+        .unfold();
+        assert_eq!(row.count_possibilities(), 2500);
+    }
+
+    #[test]
+    pub fn count_possibilities_unfolded_last_example_row() {
+        let row = SpringRow {
+            row: "?###????????".to_string(),
+            groups: vec![3, 2, 1],
+        }
+        .unfold();
+        assert_eq!(row.count_possibilities(), 506250);
+    }
 }
